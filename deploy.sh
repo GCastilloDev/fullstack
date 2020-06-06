d4318e0e8d664e5bf734d233d8176b6eb649172c
tavoGeek <gcnctz@gmail.com>
2020-06-06 13:09:52 -0500

    deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# navega a frontend
+cd frontend
+npm run build
+
+# regresar a la carpeta principal
+cd ..
+
+# copiar carpet dist
+npm run copyDist
+
+# Ir a la carpeta backend
+cd backend
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/GustavoCastilloMX/fullstack.git master:deploy
